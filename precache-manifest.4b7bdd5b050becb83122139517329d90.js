ebe2efd5f98b09f3e2f86ff89057fcd76fb76d8f
Rafler <amuronyk@gmail.com>
2019-10-24 17:18:23 +0300

    Updates

---
+++ precache-manifest.4b7bdd5b050becb83122139517329d90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca805a44f3112d4c5fc05ce340bcb573",
+    "url": "/Teamvoy/index.html"
+  },
+  {
+    "revision": "7bf3189d611b1182c412",
+    "url": "/Teamvoy/static/js/2.ba0691eb.chunk.js"
+  },
+  {
+    "revision": "bdd7a3b711ee8ff71d8d",
+    "url": "/Teamvoy/static/js/main.e3d5fd03.chunk.js"
+  },
+  {
+    "revision": "117ab2f2c4304646dc5f",
+    "url": "/Teamvoy/static/js/runtime~main.e03abb51.js"
+  }
+]);
